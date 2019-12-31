0418633e8dcff7688f1f2b2ed506b95325a96416
Dani <danimatton@gmail.com>
2019-12-31 09:15:45 -0700

    few updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
 stark = House.create(name: "Stark")
 lannister = House.create(name: "Lannister")
 
